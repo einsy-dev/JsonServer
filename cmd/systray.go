854623c85f8c2113053b750347f69df9d48d52cb
Einsy <eins.yeager@gmail.com>
2025-07-29 11:49:53 +0400

    update icon 2

--- cmd/systray.go
+++ cmd/systray.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"time"
 
 	"fyne.io/systray"
-	"fyne.io/systray/example/icon"
 )
 
 func SysTray() {
@@ -22,80 +22,37 @@ func addQuitItem() {
 	mQuit.Enable()
 	go func() {
 		<-mQuit.ClickedCh
-		fmt.Println("Requesting quit")
 		systray.Quit()
-		fmt.Println("Finished quitting")
 	}()
-	systray.AddSeparator()
 }
 
+//go:embed icon.ico
+var icon []byte
+
 func onReady() {
-	systray.SetTemplateIcon(icon.Data, icon.Data)
-	systray.SetTitle("Awesome App")
-	systray.SetTooltip("Lantern")
+	systray.SetTemplateIcon(icon, icon)
+	systray.SetTitle("Local Server")
+	systray.SetTooltip("Local Server")
+
+	clear := systray.AddMenuItem("Clear", "Clear Saved Data")
+	systray.AddSeparator()
+
+	restart := systray.AddMenuItem("Restart", "Restart Server")
+	systray.AddSeparator()
+
 	addQuitItem()
 
-	// We can manipulate the systray in other goroutines
 	go func() {
-		systray.SetTemplateIcon(icon.Data, icon.Data)
-		systray.SetTitle("Awesome App")
-		systray.SetTooltip("Pretty awesome棒棒嗒")
-		mChange := systray.AddMenuItem("Change Me", "Change Me")
-		mChecked := systray.AddMenuItemCheckbox("Checked", "Check Me", true)
-		mEnabled := systray.AddMenuItem("Enabled", "Enabled")
-		// Sets the icon of a menu item. Only available on Mac.
-		mEnabled.SetTemplateIcon(icon.Data, icon.Data)
-
-		systray.AddMenuItem("Ignored", "Ignored")
-
-		subMenuTop := systray.AddMenuItem("SubMenuTop", "SubMenu Test (top)")
-		subMenuMiddle := subMenuTop.AddSubMenuItem("SubMenuMiddle", "SubMenu Test (middle)")
-		subMenuBottom := subMenuMiddle.AddSubMenuItemCheckbox("SubMenuBottom - Toggle Panic!", "SubMenu Test (bottom) - Hide/Show Panic!", false)
-		subMenuMiddle.AddSeparator()
-		subMenuBottom2 := subMenuMiddle.AddSubMenuItem("SubMenuBottom - Panic!", "SubMenu Test (bottom)")
-
-		systray.AddSeparator()
-		mToggle := systray.AddMenuItem("Toggle", "Toggle some menu items")
-		shown := true
-		toggle := func() {
-			if shown {
-				subMenuBottom.Check()
-				subMenuBottom2.Hide()
-				mEnabled.Hide()
-				shown = false
-			} else {
-				subMenuBottom.Uncheck()
-				subMenuBottom2.Show()
-				mEnabled.Show()
-				shown = true
-			}
-		}
-		mReset := systray.AddMenuItem("Reset", "Reset all items")
 
 		for {
 			select {
-			case <-mChange.ClickedCh:
-				mChange.SetTitle("I've Changed")
-			case <-mChecked.ClickedCh:
-				if mChecked.Checked() {
-					mChecked.Uncheck()
-					mChecked.SetTitle("Unchecked")
-				} else {
-					mChecked.Check()
-					mChecked.SetTitle("Checked")
-				}
-			case <-mEnabled.ClickedCh:
-				mEnabled.SetTitle("Disabled")
-				mEnabled.Disable()
-			case <-subMenuBottom2.ClickedCh:
-				panic("panic button pressed")
-			case <-subMenuBottom.ClickedCh:
-				toggle()
-			case <-mReset.ClickedCh:
+			case <-clear.ClickedCh:
+				clear.SetTitle("Cleared")
+
+			case <-restart.ClickedCh:
 				systray.ResetMenu()
 				addQuitItem()
-			case <-mToggle.ClickedCh:
-				toggle()
+
 			}
 		}
 	}()
